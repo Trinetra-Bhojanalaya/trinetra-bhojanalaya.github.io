d8ea9731fb400cdb3196e9219dcbbacc94f4ffc8
Trinetra-Bhojanalaya <becomingrohit@gmail.com>
2025-12-08 23:38:35 +0530

    Update cart.js

--- assets/cart.js
+++ assets/cart.js
@@ -1,1 +1,18 @@
+let cart = [];
+
+export function addToCart(item, price) {
+    cart.push({ item, price });
+}
+
+export function getCartTotal() {
+    return cart.reduce((sum, x) => sum + x.price, 0);
+}
+
+export function getCartText() {
+    return cart.map(x => `${x.item} - ₹${x.price}`).join("\n");
+}
+
+export function clearCart() {
+    cart = [];
+}
 
