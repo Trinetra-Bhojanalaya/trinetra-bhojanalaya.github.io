e620c820f9ce36104f0ce3c0996b438095e2bd52
Trinetra-Bhojanalaya <becomingrohit@gmail.com>
2025-12-09 00:03:31 +0530

    Update saveOrder.js

--- assets/saveOrder.js
+++ assets/saveOrder.js
@@ -1,19 +1,21 @@
 import { db } from "./firebase.js";
 import { collection, addDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-export async function saveOrder(name, order, type, address, lat, lon) {
+export async function saveOrder(name, cart, type, address, lat, lon, subtotal, deliveryCharge, total) {
     try {
         await addDoc(collection(db, "orders"), {
             name,
-            order,
+            cart,
             type,
             address,
             gpsLat: lat,
             gpsLon: lon,
+            subtotal,
+            deliveryCharge,
+            total,
             timestamp: serverTimestamp()
         });
     } catch (err) {
         console.error("Error saving order:", err);
     }
 }
-
