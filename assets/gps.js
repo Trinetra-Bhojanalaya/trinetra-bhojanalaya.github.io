ff5c8312d131b05acd8add57cc9d5ac1d728f651
Trinetra-Bhojanalaya <becomingrohit@gmail.com>
2025-12-08 23:38:21 +0530

    Update gps.js

--- assets/gps.js
+++ assets/gps.js
@@ -1,1 +1,20 @@
+export function askForGPS(callbackSuccess, callbackError) {
+    if (!navigator.geolocation) {
+        callbackError("GPS not supported on this device.");
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        pos => {
+            callbackSuccess({
+                lat: pos.coords.latitude,
+                lon: pos.coords.longitude
+            });
+        },
+        err => {
+            callbackError("GPS permission denied.");
+        },
+        { enableHighAccuracy: true }
+    );
+}
 
